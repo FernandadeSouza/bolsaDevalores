e2872d4804a91c96405aa2152c055397d01267db
STEPHANIE MARYS DA SILVA <stephanie.mmarys01@gmail.com>
2023-07-25 18:54:13 -0300

    Criei um branch com o meu nome.

---
+++ Aula1/Aula1/Form2.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Aula1
+{
+    partial class frmInvest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(265, 36);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(249, 31);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Investidor Iniciante ";
+            // 
+            // frmInvest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 655);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmInvest";
+            this.Text = "Investidor Iniciante ";
+            this.Load += new System.EventHandler(this.frmInvest_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
---
+++ Aula1/Aula1/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula1
+{
+    public partial class frmInvest : Form
+    {
+        public frmInvest()
+        {
+            InitializeComponent();
+        }
+
+        private void frmInvest_Load(object sender, EventArgs e)
+        {
+            string[] vetortutorial = new string[3];
+            vetortutorial[0] = " O que é a Bolsa de Valores? \n É um ambiente onde são negociados ações,títulos"
+             [1] = 
+        }
+    }
+}
