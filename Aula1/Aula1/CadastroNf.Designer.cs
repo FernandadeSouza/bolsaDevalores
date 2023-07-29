8791ae65874720f208aac220a280380bfe28db1b
nicole.fsantos6 <nicole.fsantos6@CAR0588313W10-1>
2023-07-28 21:30:09 -0300

    alterei imagem

--- Aula1/Aula1/CadastroNf.Designer.cs
+++ Aula1/Aula1/CadastroNf.Designer.cs
@@ -29,12 +29,184 @@ namespace Aula1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.lblCadastro = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dpNascimento = new System.Windows.Forms.Label();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnAvancar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCadastro
+            // 
+            this.lblCadastro.AutoSize = true;
+            this.lblCadastro.BackColor = System.Drawing.Color.Silver;
+            this.lblCadastro.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastro.ForeColor = System.Drawing.Color.Black;
+            this.lblCadastro.Location = new System.Drawing.Point(342, 9);
+            this.lblCadastro.Name = "lblCadastro";
+            this.lblCadastro.Size = new System.Drawing.Size(199, 36);
+            this.lblCadastro.TabIndex = 0;
+            this.lblCadastro.Text = "Cadastre - se ";
+            this.lblCadastro.Click += new System.EventHandler(this.lblCadastro_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.Color.White;
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(99, 167);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(467, 31);
+            this.txtNome.TabIndex = 2;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // dpNascimento
+            // 
+            this.dpNascimento.AutoSize = true;
+            this.dpNascimento.BackColor = System.Drawing.Color.Silver;
+            this.dpNascimento.Font = new System.Drawing.Font("Arial Unicode MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dpNascimento.Location = new System.Drawing.Point(95, 222);
+            this.dpNascimento.Name = "dpNascimento";
+            this.dpNascimento.Size = new System.Drawing.Size(205, 25);
+            this.dpNascimento.TabIndex = 3;
+            this.dpNascimento.Text = "Data de Nascimento";
+            // 
+            // txtData
+            // 
+            this.txtData.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtData.Location = new System.Drawing.Point(88, 322);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(478, 31);
+            this.txtData.TabIndex = 4;
+            this.txtData.TextChanged += new System.EventHandler(this.txtData_TextChanged);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Silver;
+            this.lblEmail.Font = new System.Drawing.Font("Arial Unicode MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(87, 294);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(65, 25);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(89, 250);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(477, 31);
+            this.txtEmail.TabIndex = 6;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.BackColor = System.Drawing.Color.Silver;
+            this.lblSenha.Font = new System.Drawing.Font("Arial Unicode MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(87, 380);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(72, 25);
+            this.lblSenha.TabIndex = 7;
+            this.lblSenha.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(88, 408);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(478, 31);
+            this.txtSenha.TabIndex = 8;
+            this.txtSenha.TextChanged += new System.EventHandler(this.txtSenha_TextChanged);
+            // 
+            // btnAvancar
+            // 
+            this.btnAvancar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAvancar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAvancar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAvancar.Location = new System.Drawing.Point(348, 575);
+            this.btnAvancar.Name = "btnAvancar";
+            this.btnAvancar.Size = new System.Drawing.Size(144, 51);
+            this.btnAvancar.TabIndex = 9;
+            this.btnAvancar.Text = "Avançar ";
+            this.btnAvancar.UseVisualStyleBackColor = false;
+            this.btnAvancar.Click += new System.EventHandler(this.btnAvancar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 22);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nome completo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Silver;
+            this.label2.Font = new System.Drawing.Font("Arial Unicode MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(221, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(414, 42);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Para começar, informe os dados que você vai usar\r\npara acessar sua conta CodeCash" +
+    ".";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(933, 655);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAvancar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.dpNascimento);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblCadastro);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCadastro;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label dpNascimento;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnAvancar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Aula1/Aula1/CadastroNf.cs
+++ Aula1/Aula1/CadastroNf.cs
@@ -16,5 +16,56 @@ namespace Aula1
         {
             InitializeComponent();
         }
+
+        private void btnAvancar_Click(object sender, EventArgs e)
+        {
+            string nome = Convert.ToString(txtNome.Text);
+            string data = Convert.ToString(txtData.Text);
+            string email = Convert.ToString(txtEmail.Text);
+            string senha = Convert.ToString(txtSenha.Text);
+
+            Form frmquiz = new Form1();
+
+            //frmquiz.show();
+
+
+
+
+
+
+            
+
+
+
+
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtData_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void lblCadastro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            string dataNascimento = dpDataNascimento.Value.ToString("yyyy-MM-dd");
+            DateTime today = DateTime.Now;
+            DateTime birthDate = dpDataNascimento.Value;
+            int age = (int)((today - birthDate).TotalDays / 365.25);
+        }
     }
 }
