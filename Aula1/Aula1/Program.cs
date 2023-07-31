4b745b1a7d8e5ef70e5ec6e757b0a71151b2b10e
Samantha Canin <samanthapirescanin1@gmail.com>
2023-07-31 20:25:10 -0300

    coreção tela login

--- Aula1/Aula1/Program.cs
+++ Aula1/Aula1/Program.cs
@@ -16,7 +16,7 @@ namespace Aula1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TelaLogin());
+            Application.Run(new frmLogin());
         }
     }
 }
