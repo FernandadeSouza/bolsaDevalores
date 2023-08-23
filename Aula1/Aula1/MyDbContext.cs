cc404ef3b28918ecf70eeedcb29ac5c678c35378
FERNANDA RODRIGUES FARIA DE SOUZA <fernanda.rfsouza1@senacsp.edu.br>
2023-08-23 19:33:49 -0300

    inserindo novamente form git

--- Aula1/Aula1/MyDbContext.cs
+++ Aula1/Aula1/MyDbContext.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Aula1
 {
-    class MyDbContext
+    class MyDbContext:DbContext
+
     {
+        public MyDbContext() : base ("name=MyDbContext") { }
     }
 }
