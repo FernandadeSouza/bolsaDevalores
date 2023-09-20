c203c4507589fd2a4d395f9c2a85ca81b468941c
FERNANDA RODRIGUES FARIA DE SOUZA <fernanda.rfsouza1@senacsp.edu.br>
2023-09-20 18:20:45 -0300

    alteração designer

--- Aula1/Aula1/CadastroNf.Designer.cs
+++ Aula1/Aula1/CadastroNf.Designer.cs
@@ -123,7 +123,7 @@ namespace Aula1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(764, 422);
+            this.pictureBox1.Location = new System.Drawing.Point(735, 427);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(28, 29);
             this.pictureBox1.TabIndex = 14;
